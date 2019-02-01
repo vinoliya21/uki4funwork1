0dc6cca24073f3c77b9bf7633421e419754fdc80
vinoliya21 <vinoliyakavignan1997@gmail.com>
2019-02-01 12:42:21 +0530

    funwork

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-SC.initialize({
-  client_id: '340f063c670272fac27cfa67bffcafc4'
-});
+ SC.initialize({
+   client_id: '340f063c670272fac27cfa67bffcafc4'
+ });
 
-$(document).ready(function() {
-  SC.stream('/tracks/496789389',function(sound){
-    $('#start').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
-         $('#stop').click(function(e) {
-           e.preventDefault();
-           sound.stop();
-         });
-       });
+ $(document).ready(function() {
 
+   SC.stream('/tracks/496789389',function(sound){
+
+     $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+          $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });
+        });
  });
