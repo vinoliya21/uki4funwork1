87137f917b38a6dd72007a02807b33f69d3cc157
vinoliya21 <vinoliyakavignan1997@gmail.com>
2019-02-11 09:51:31 +0530

    funwork1

---
+++ js/main.js
@@ -0,0 +1,321 @@
+
+SC.initialize({
+  client_id: 'a3e059563d7fd3372b49b37f00a00bcf'
+
+});
+
+$(document).ready(function() {
+/* 24 naan un */
+  SC.stream('/tracks/346104528', function(sound) {
+    $('#start').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+/* 2.0*/
+  SC.stream('/tracks/354663899', function(sound) {
+    $('#start1').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop1').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+/* senjitaley */
+  SC.stream('/tracks/273543641', function(sound) {
+    $('#start2').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop2').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* Addchu thukku*/
+  SC.stream('/tracks/543426369', function(sound) {
+    $('#start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop3').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* high onlove */
+  SC.stream('/tracks/456435057', function(sound) {
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop4').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* thalli pogahey*/
+  SC.stream('/tracks/242542934', function(sound) {
+    $('#start5').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop5').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+/* neeyum naanum  */
+
+  SC.stream('/tracks/496210638', function(sound) {
+    $('#start6').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop6').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* kathakali*/
+  SC.stream('/tracks/245201802', function(sound) {
+    $('#start8').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop8').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* vadi pulla vadi*/
+  SC.stream('/tracks/163228934', function(sound) {
+    $('#start9').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop9').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+/* rowdy baby*/
+  SC.stream('/tracks/536634576', function(sound) {
+    $('#start10').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop10').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+/* new york nagaram*/
+  SC.stream('/tracks/113398876', function(sound) {
+    $('#start7').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop7').click(function(e) {
+      e.preventDefault();
+      sound.pause();
+    });
+  });
+
+
+
+
+  SC.stream('/tracks/557856666', function(sound) {
+    $('#start11').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop11').click(function(e) {
+      e.preventDefault();
+      sound.pause();
+    });
+  });
+
+/* english Songs*/
+
+/* shape of you*/
+SC.stream('/tracks/303123954', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+/* Dalinda*/
+SC.stream('/tracks/89145752', function(sound) {
+  $('#estart1').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop1').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+/* love*/
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart2').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop2').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+/* get on the floor*/
+SC.stream('/tracks/181436429', function(sound) {
+  $('#estart3').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop3').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+/* inter love*/
+SC.stream('/tracks/45292719', function(sound) {
+  $('#estart4').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop4').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+/*nina*/
+SC.stream('/tracks/114776547', function(sound) {
+  $('#estart5').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop5').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+SC.stream('/tracks/27432487', function(sound) {
+  $('#estart').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+  $('#estop').click(function(e) {
+    e.preventDefault();
+    sound.stop();
+  });
+});
+
+
+});
